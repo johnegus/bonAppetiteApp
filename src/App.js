8eb4ffd1d043675d7c16e3af05a0a15b52e985cf
john <jgh2102@gmail.com>
2020-10-14 13:41:58 +0900

    interpolated recipe api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 
-function App() {
-  return (
-    <h1>Hello world!</h1>
+const App = () => {
+
+  const APP_ID = 'd52ce565';
+  const APP_KEY = 'ae866e8bb3383dcb7a0c888c9af8aec9';
+  const exampleReq = 
+`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+const [counter, setCounter] = useState(0);
+useEffect(()=>{
+  console.log('Effect has been run')
+});
+
+  return(
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button onClick={() => setCounter(counter + 1)}
+        className="search-button" 
+        type="submit">{counter}</button>
+      </form>
+    </div>
   );
-}
+};
 
 export default App;
